b1b72815859da493ed0fb150c3612e37c11df2c7
SorrowPhage <470565129@qq.com>
2024-02-28 12:53:59 +0800

    族群详细信息

--- src/utils/hasPermissions.js
+++
@@ -1,6 +0,0 @@
-import store from '../store'
-
-export function hasPermissions(permissions) {
-    const myPermissions = store.state.CzpUser.permissions;
-    return myPermissions.indexOf(permissions) > -1;
-}
